02ebfe4ade5677344143461d6bcd328f17661965
Ertugrul Erdem <eerdem@amazon.com>
2025-10-10 09:55:35 +0000

    change perf tests name to attempt to unstuck github workflow

--- load-generator/tcp-tester/src/bin/tcp-tester/ebpf_loader.rs
+++ load-generator/tcp-tester/src/bin/tcp-tester/ebpf_loader.rs
@@ -3,13 +3,9 @@ use aya_log::EbpfLogger;
 
 pub fn load_ebpf_program() -> Result<Ebpf, String> {
     #[cfg(debug_assertions)]
-    let bpf = Ebpf::load(include_bytes_aligned!(concat!(
-        env!("BPF_OBJECT_PATH")
-    )));
+    let bpf = Ebpf::load(include_bytes_aligned!(concat!(env!("BPF_OBJECT_PATH"))));
     #[cfg(not(debug_assertions))]
-    let mut bpf = Ebpf::load(include_bytes_aligned!(concat!(
-        env!("BPF_OBJECT_PATH")
-    )));
+    let mut bpf = Ebpf::load(include_bytes_aligned!(concat!(env!("BPF_OBJECT_PATH"))));
     match bpf {
         Ok(mut ebpf_program) => {
             if let Err(error) = EbpfLogger::init(&mut ebpf_program) {
